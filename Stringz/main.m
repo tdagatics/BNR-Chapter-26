c5c35cfbf2178a2c5004cc6ed2a5eb5940f68ad8
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-01 20:51:45 -0700

    Updated BNR-Chapter-26 commit to account for reading text files

--- Stringz/main.m
+++ Stringz/main.m
@@ -14,13 +14,27 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
+        
+        // Writing files to text file
+        
         NSMutableString *string = [[NSMutableString alloc] init];
         for (int i = 0; i < 10; i++) {
             [string appendString:@"Testing this loop\n"];
         }
         [string writeToFile:@"/tmp/loop.txt" atomically:YES encoding:NSUTF8StringEncoding error:NULL];
         NSLog(@"Done writing /tmp/loop.txt");
+    
+        // Reading from a text file
+    
+        NSError *error;
+        NSString *str = [[NSString alloc] initWithContentsOfFile:@"/tmp/temporary.txt" encoding:NSUTF8StringEncoding error:&error];
+        
+        if (!str) {
+            NSLog(@"Read filed: %@", [error localizedDescription]);
+        } else {
+            NSLog(@"temporary.txt looks lie this:\n %@", str);
         }
+    }
     return 0;
 }
 
